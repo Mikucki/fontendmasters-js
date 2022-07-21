28c372c1e4e6278b44bf6d7809cabf0e87d66e08
Mikucki <piotr.mikucki@op.pl>
2022-07-21 16:38:48 -0400

    css project

---
+++ main.js
@@ -0,0 +1,15 @@
+let places = {
+    kitchen: false,
+    badroom: false,
+    livingroom: true
+}
+
+function beenSeen(places){
+    if places === true
+        let czemuja = new [];
+
+    return czemuja
+};
+
+_.filter(places, beenSeen);
+
